1e29bbcf4382dd99bb0816827afcceec0e77ef25
Vitalii <Vitalii@DESKTOP-I6T8KRU>
2024-04-29 09:41:52 +0300

    Add project files.

---
+++ Cinema.Core/Domain/Entities/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinema.Core.Domain.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Trailers { get; set; } = null!;
+        public string Actors { get; set; } = null!;
+        public string Genre { get; set; } = null!;
+        public DateTime Duration { get; set; }
+        public double Rating { get; set; }
+        public int SeanceId {  get; set; }
+        [ForeignKey(nameof(SeanceId))]
+        public Seance? seance { get; set; }
+
+    }
+}
