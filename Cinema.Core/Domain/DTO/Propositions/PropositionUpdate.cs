a8f2c749f23bb736e9397862c247d01c221c7c8f
Yegorchyk <127146240+Yegorchyk@users.noreply.github.com>
2024-05-14 13:07:14 +0300

    MyDTO

---
+++ Cinema.Core/Domain/DTO/Propositions/PropositionUpdate.cs
@@ -0,0 +1,48 @@
+﻿using Cinema.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Core.Domain.DTO.Propositions
+{
+    internal class PropositionUpdate
+    {
+
+
+        public PropositionUpdate(int id, string description, decimal discount, int movieId, Movie? Movie) 
+        {
+            Id = id;
+            Description = description;
+            Discount = discount;
+            MovieId = movieId;
+            movie = Movie;
+
+        }
+        public int Id { get; set; }
+        public string Description { get; set; } = null!;
+        public decimal Discount { get; set; }
+        public int MovieId { get; set; }
+        [ForeignKey(nameof(MovieId))]
+        public Movie? movie { get; set; }
+
+        public string Title { get; set; }
+
+
+        public Proposition ToProposition()
+        {
+            var prop = new Proposition()
+            {
+                Id = this.Id,
+                Description = this.Description,
+                Discount = this.Discount,
+                MovieId = this.MovieId,
+                movie = this.movie
+            };
+
+            return prop;
+        }
+    }
+}
---
+++ Cinema.Core/Domain/DTO/User/UserResponse.cs
@@ -0,0 +1,70 @@
+﻿using Cinema.Core.Domain.DTO.Propositions;
+using Cinema.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Cinema.Core.Domain.DTO.User
+{
+    public class UserResponse
+    {
+
+        public ICollection<Ticket> tickets { get; set; }
+        public ICollection<Proposition> propositions { get; set; }
+        public UserResponse()
+        {
+            foreach (var ticket in tickets)
+            {
+               this.tickets = new List<Ticket>();
+            }
+
+            foreach (var prop in propositions)
+            {
+                this.propositions = new List<Proposition>();
+            }
+
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override string? ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public UserADD ToAddRequest()
+        {
+            return new UserADD(tickets, propositions);
+        }
+    }
+
+    public static class UserExtensions
+    {
+        public static UserResponse ToUserResponse(this CinemaUser user)
+        {
+            return new UserResponse
+            {
+                tickets = user.tickets,
+                propositions = user.propositions
+            };
+
+            /*   foreach (var ticket in user.tickets)
+               {
+                   response.tickets.Add(ticket.ToTicket());
+               }
+
+               foreach (var prop in user.propositions)
+               {
+                   response.propositions.Add(prop.ToProposition());
+               }*/
+
+            // return response;
+        }
+    }
+}
