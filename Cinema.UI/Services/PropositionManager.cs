13e8444ae8623291f6524c5042696df1821efab2
Yegorchyk <127146240+Yegorchyk@users.noreply.github.com>
2024-05-14 13:11:17 +0300

    MyServices

---
+++ Cinema.UI/Services/PropositionManager.cs
@@ -0,0 +1,93 @@
+﻿using Cinema.Core.Domain.DTO.Propositions;
+using Cinema.Core.Domain.ServiceContracts;
+using Cinema.Infrastructure.DBContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Cinema.Core.Domain.Sefvices
+{
+    internal class PropositionManager : IPropositionService_
+    {
+        readonly ApplicationDbContext _context;
+        public PropositionManager(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PropositionResponse?> AddPropositionAsync(AddProposition? addProp)
+        {
+            if (addProp == null) throw new ArgumentNullException(nameof(addProp));
+
+            if (addProp.UserId == Guid.Empty) throw new ArgumentException(nameof(addProp.UserId));
+            if (addProp.MovieId < 0) throw new ArgumentException(nameof(addProp.MovieId));
+
+            if ((await _context.CinemaUsers.Include(x => x.Propositions).Where(x => x.Id == addProp.UserId).FirstAsync())
+                .Propositions.Where(x => x.MovieId == addProp.MovieId).Count() > 0)
+            {
+                return null;
+            }
+
+          
+
+            var newProposition = await _context.propositions.AddAsync(new Entities.Proposition()
+            {
+                Description = addProp.Description,
+                MovieId = addProp.MovieId,
+                Discount = addProp.Discount
+            });
+
+            await _context.SaveChangesAsync();
+            await newProposition.Reference(x => x.Movie).LoadAsync();
+            await newProposition.Reference(x => x.Description).LoadAsync();
+            return newProposition.Entity.ToPropositionResponse();
+        }
+
+        public async Task<bool> ClearPropositionAsync(Guid? UserID)
+        {
+            if (UserID == null) throw new ArgumentNullException(nameof(UserID));
+
+            if ((await _context.propositions.FindAsync(UserID.ToString())) == null) throw new ArgumentException(nameof(UserID));
+
+           
+            return true;
+        }
+
+        public async Task<List<PropositionResponse>> GetAllPropositionssAsync(Guid? UserID)
+        {
+            if (UserID == null) throw new ArgumentNullException(nameof(UserID));
+
+            if ((await _context.CinemaUsers.FindAsync(UserID.ToString())) == null) throw new ArgumentException(nameof(UserID));
+
+            return (await _context.propositions.Include(x => x.Id).Where(x => x.UserId == UserID).ToListAsync()).Select(x => x.ToUserResponse()).ToList();
+        }
+
+        public async Task<PropositionResponse>? GetPropositionAsync(int propositionID)
+        {
+            if (propositionID < 0) throw new ArgumentException($"CartProductId {propositionID} can't be lower than 0");
+
+            var Proposition = await _context.propositions.Include(x => x.Movie).ThenInclude(x => x.Description).Where(x => x.Id == propositionID).FirstAsync();
+            if (Proposition == null) throw new ArgumentException(nameof(propositionID));
+
+            return Proposition.ToCartPropositionResponse();
+        }
+
+        public async Task<bool> RemovePropositionAsync(PropositionResponse? proposition)
+        {
+            if (proposition == null) throw new ArgumentNullException(nameof(proposition));
+            if (proposition.MovieId <0) throw new ArgumentException(nameof(proposition.MovieId));
+
+            var dbProposition = await _context.propositions.Where(x => x.Id == proposition.Id).FirstOrDefaultAsync();
+            if (dbProposition == null) return false;
+
+            _context.propositions.Remove(dbProposition);
+            await _context.SaveChangesAsync();
+            return true;
+
+        }
+      
+
+    }
+}
---
+++ Cinema.UI/Services/UserManager.cs
@@ -0,0 +1,52 @@
+﻿using Cinema.Core.Domain.DTO.User;
+using Cinema.Core.Domain.ServiceContracts;
+using Cinema.Infrastructure.DBContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.UI.Services
+{
+    public class UserManager : IUserService
+    {
+        readonly ApplicationDbContext _context;
+        readonly IPropositionService_ _propService;
+        public UserManager(ApplicationDbContext context, IPropositionService_ propositionService)
+        {
+            _context = context;
+            _propService = propositionService;
+        }
+        public async Task<UserResponse> AddUser(UserADD aDD)
+        {
+            if (aDD == null) throw new ArgumentNullException(nameof(aDD));
+            if (aDD.id == Guid.Empty) throw new ArgumentException(nameof(aDD.id));
+
+          //  aDD.propositions = await _propService.GetAllPropositionssAsync(aDD.id);
+          var dbUser = aDD.ToCinemaUser();
+            await _context.CinemaUsers.AddAsync(dbUser);
+            await _context.SaveChangesAsync();
+
+            return dbUser.ToUserResponse();
+
+        }
+
+        public async Task DeleteUserAsync(int id)
+        {
+            _context.Tickets.Remove(await _context.CinemaUsers.FindAsync(id) ?? throw new ArgumentException("Invalid User response"));
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<UserResponse>? GetUserAsync(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserExistAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserResponse> UpdateUserAsync(UserUpdate update)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
