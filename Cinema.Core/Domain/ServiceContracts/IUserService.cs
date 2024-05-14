087d9a5e4f83b30f98d16fd6590c7bc7685c2511
Yegorchyk <127146240+Yegorchyk@users.noreply.github.com>
2024-05-14 13:08:39 +0300

    MyServices

---
+++ Cinema.Core/Domain/ServiceContracts/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using Cinema.Core.Domain.DTO.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Core.Domain.ServiceContracts
+{
+    public interface IUserService
+    {    
+        public Task<UserResponse> AddUser(UserADD aDD);
+        public Task<UserResponse>? GetUserAsync(int UserId);
+        public Task<UserResponse> UpdateUserAsync(UserUpdate update);
+        public Task<bool> IsUserExistAsync(int id);
+        public Task DeleteUserAsync(int id);
+
+       
+
+    }
+}
