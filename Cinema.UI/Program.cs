b299587cbd3ab9c484cca91985f5a85aa26c1483
Vitalii <Vitalii@DESKTOP-I6T8KRU>
2024-05-18 21:55:05 +0300

    fix

--- Cinema.UI/Program.cs
+++ Cinema.UI/Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString, b => b.MigrationsAssembly("Cinema.Infrastructure")));
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
